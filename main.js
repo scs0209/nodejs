bf9e1503df4610ae523c04cec87a9b390bc12575
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-08-21 01:21:33 +0900

    main.js의 코드를 간단하게 정리해줌

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -16,69 +43,17 @@ var app = http.createServer(function(request,response){
           fs.readdir('./data', function(err, filelist){
             var title = 'Welcome';
             var description = 'Hello, Node.js';
-            /*
-            var list = `<ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>`;
-            */
-            var list = '<ul>';
-
-            var i = 0;
-            while(i < filelist.length){
-              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-              i = i + 1;
-            }
-            list = list + '</ul>';
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist); //매개변수 filelist는 data폴더에 있는 file의 list를 가르킨다
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           })
-
-        //queryData.id의 값이 undefined라면 위의 코드가 실행 될것이고 아니라면 else에 있는 코드들이 실행되게 설정해놓은 코드들이다.
       } else {
         fs.readdir('./data', function(err, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
           fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           });
